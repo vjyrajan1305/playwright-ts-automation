7e57e549ff76cf941e445e8631cf274359fa3da5
vjyrajan1305 <vjyrajan@gmail.com>
2026-02-10 13:02:36 +0530

    Add files via upload

--- tests/firsttest.spec.ts
+++ tests/firsttest.spec.ts
@@ -1,14 +1,12 @@
 import { test, expect } from '@playwright/test';
 
 test('Google Search test', async ({ page }) => {
-  await page.goto('https://www.google.com');
+  await page.goto('https://letcode.in/edit');
 
   // type into the search box
-  await page.locator('input[name="q"]').fill('Playwright testing');
+  await page.locator('input[id="fullName"]').fill('Playwright testing');
 
-  // click search
-  await page.keyboard.press('Enter');
+  // assert that the search box contains the text
+  await expect(page.getByRole('button', { name: 'Enter your full Name' })).toContainText('Playwright testing'); 
 
-  // check results page title
-  await expect(page).toHaveTitle(/Playwright/);
 });
